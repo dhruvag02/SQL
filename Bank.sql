df1d4d8305a66c1c39b91dcb7fa476037e89a0de
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-05-13 22:23:53 +0530

    Update Bank.sql

--- Bank.sql
+++ Bank.sql
@@ -62,3 +62,65 @@ insert into loan values(2,"SBI-ResidencyRoad",2000);
 insert into loan values(3,"SBI-ShivajiRoad",3000);
 insert into loan values(4,"SBI-parliamentRoad",4000);
 insert into loan values(5,"SBI-Jantarmantar",5000);
+                                       
+# Query
+select D.*
+from branch B, bankaccount A, depositer D
+where A.accno=D.accno and A.branchname=B.branchname and A.branchname='SBI-RESIDENCYROAD'
+group by D.customername
+having count(D.accno)>=2;
+# Alternate
+select C.customername
+from bankcustomer C
+where exists(select D.customername, count(D.customername)
+			 from depositer D, bankaccount A
+             where D.accno=A.accno and C.customername=D.customername and A.branchname='SBI-RESIDENCYROAD'
+             group by D.customername
+             having count(D.customername)>=2);
+             
+create view MAX_LOAN1(branchname, amount)as
+select branchname, sum(amount)
+from loan
+group by branchname;
+select *
+from MAX_LOAN1;
+drop view MAX_LOAN1;
+
+select BC.customername
+from bankcustomer BC
+where not exists(select branchname from branch where branchcity='Delhi' not in(
+select BA.branchname from depositer D, bankaccount BA where D.accno=BA.accno and BC.customername=D.customername));
+                  
+select D.customername 
+from depositer D, bankaccount A
+where D.accno=A.accno and A.branchname='SBI-ResidencyRoad'
+group by D.customername
+having count(D.accno)>=2;
+
+select branchname, branchcity, 1.05*assests as newassets
+from branch;
+
+(select C.customername
+from bankcustomer C
+where C.customername in(select D.customername
+						from depositer D))
+union
+(select D.customername
+from depositer D,bankaccount A,branch B
+where D.accno=A.accno and A.branchname=B.branchname and B.branchname in(select L.branchname
+																		from loan L)); 
+                                                                        
+select distinct(C.customername)
+from bankcustomer C
+where not exists(select branchname
+				 from branch 
+                 where branchcity='Delhi' and branchname not in
+                 (select A.branchname
+						from bankaccount A, depositer D
+                        where A.accno=D.accno and C.customername=D.customername));
+                        
+(select C.customername
+from bankcustomer C)
+union
+(select B.customername
+from borrower B);                  
